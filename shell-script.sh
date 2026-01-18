532b3c1faaad278f4a568bb3254b7c8e0b97df48
Ubuntu <ubuntu@ip-172-31-41-1.us-east-2.compute.internal>
2026-01-18 18:40:58 +0000

    This is first commit in git

---
+++ shell-script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+############################
+# Author Praveen
+# Date 01/17/2026
+# This Script Runs the output of node health
+# Version V1
+# ##########################
+set -x # debug mode
+set -e # exit the script when there is an error 
+set -o pipefail 
+
+aws s3 ls
+
+aws ec2 describe-instances
+
+aws lambda list-functions
+
