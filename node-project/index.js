5243ae82fbcff4b7b18c60b623d198aa5327deed
praveengadiparthi-max <praveengadiparthi97@gmail.com>
2026-02-01 14:25:11 -0500

    Update comment in index.js

--- node-project/index.js
+++ node-project/index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const PORT = 3000;
-//test12
+//test123
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello from Node.js DevOps learning app 🚀\n');
